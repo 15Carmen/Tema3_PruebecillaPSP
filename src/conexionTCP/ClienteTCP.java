4626e4a4fa4214987a586e29dfb0a9599b1a5f98
15Carmen <carmen.martin@iesnervion.es>
2023-01-10 14:40:45 +0100

    Initial commit

---
+++ src/conexionTCP/ClienteTCP.java
@@ -0,0 +1,44 @@
+package conexionTCP;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClienteTCP {
+
+
+    public static void main(String[] args) {
+        try {
+            //1.- Creacion del socket de tipo cliente
+            System.out.println("(Cliente): Creacion del socket...");
+            InetAddress direccion= InetAddress.getLocalHost();
+            Socket socketCliente = new Socket(direccion, 50000);
+
+            //2.- Abrir flujos de lectura y escritura
+            System.out.println("(Cliente): Apertura de flujos de entrada y salida...");
+            OutputStream os = socketCliente.getOutputStream();
+            InputStream is = socketCliente.getInputStream();
+
+            //3.- Intercambio de datos con el servidor
+            System.out.println("(Cliente): Envia mensajae al servidor con 14...");
+            os.write(14);
+
+            System.out.println("(Cliente): Lectura del mensaje del servidor...");
+            System.out.println("Mensaje recibbido por servidor: " + is.read());
+
+            //4.- Cerrar los flujos de lectura y escritura
+            System.out.println("(Cliente): Cerramos el flujo de lectura y escritura...");
+            is.close();
+            os.close();
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/conexionTCP/ServidorTCP.java
@@ -0,0 +1,46 @@
+package conexionTCP;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTCP {
+    public static void main(String[] args) {
+        try {
+            //1.- Creacion del Socket Servidor
+            System.out.println("(Servidor): Abriendo conexion... ");
+            ServerSocket socketServidor = new ServerSocket(50000);
+
+            //2.- Esepra y acepta conexiones
+            System.out.println("(Servidor): Aceptado conexion...");
+            Socket socketCliente = socketServidor.accept();
+
+            //3.- Flujos de entrada y salida
+            System.out.println("(Servidor): Abriendo flujos de entrada y salida...");
+            InputStream is = socketCliente.getInputStream();
+            OutputStream os = socketCliente.getOutputStream();
+
+            //4.- Intercambiar datos con el cliente
+            System.out.println("(Servidor): Leo mensaje del cliente...");
+            System.out.println("Mensaje del cliente: " + is.read());
+
+            System.out.println("(Servidor): Envio mensaje al cliente con 7...");
+            os.write(7);
+
+            //5.- Cerrar flujos de lectura y escritura
+            is.close();
+            os.close();
+
+            //6.- Cerrar la conexion
+            socketServidor.close();
+            socketCliente.close();
+
+
+        }catch (IOException e){
+            System.err.println("Error: ");
+        }
+    }
+}
